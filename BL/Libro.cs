b09780a638cde3ef4e04b88b0d08a160471313b2
Efrain Ignacio <efrainignaciohernandez@gmail.com>
2022-12-09 17:41:22 -0600

    libro

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -19,9 +19,9 @@ namespace BL
             {
                 using (SqlConnection connection=new SqlConnection(DL.Conexion.Con()) )
                 {
+                    string query = "LibroAdd";
                     using (SqlCommand cmd = new SqlCommand())
                     {
-                        var query = "LibroAdd";
 
                         cmd.Connection = connection;
                         cmd.CommandText = query;
@@ -29,22 +29,28 @@ namespace BL
 
                         connection.Open();
 
-                        SqlParameter[] sqlParameter = new SqlParameter[10];
+                        SqlParameter[] sqlParameter = new SqlParameter[7];
+
                         sqlParameter[0] = new SqlParameter("Nombre", SqlDbType.VarChar);
                         sqlParameter[0].Value = libro.Nombre;
-                        sqlParameter[1] = new SqlParameter("NumeroDePaginas", SqlDbType.Int);
+
+                        sqlParameter[1] = new SqlParameter("NUmeroDePaginas", SqlDbType.Int);
                         sqlParameter[1].Value = libro.NumeroPaginas;
-                        sqlParameter[2] = new SqlParameter("FechaPublicacion", SqlDbType.Date);
+
+                        sqlParameter[2] = new SqlParameter("FechaPublicacion", SqlDbType.VarChar);
                         sqlParameter[2].Value = libro.FechaPublicacion;
+
                         sqlParameter[3] = new SqlParameter("Edicion", SqlDbType.VarChar);
                         sqlParameter[3].Value = libro.Edicion;
+
                         sqlParameter[4] = new SqlParameter("IdAutor", SqlDbType.Int);
                         sqlParameter[4].Value = libro.Autor.IdAutor;
+
                         sqlParameter[5] = new SqlParameter("IdEditorial", SqlDbType.Int);
                         sqlParameter[5].Value = libro.Editorial.IdEditorial;
+
                         sqlParameter[6] = new SqlParameter("IdGenero", SqlDbType.Int);
                         sqlParameter[6].Value = libro.Genero.IdGenero;
-
 
 
                         cmd.Parameters.AddRange(sqlParameter);
@@ -60,6 +66,59 @@ namespace BL
                 }
                 result.Correct = true;
             }
+            catch (Exception ex)
+            {
+                result.Ex = ex;
+            }
+
+            return result;
+        }
+
+        public static ML.Result Update(int IdUsuario)
+        {
+            ML.Result result = new ML.Result();
+
+
+
+
+            return result;
+        }
+        public static ML.Result Delete(int IdLibro)
+        {
+            ML.Result result=new ML.Result();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(DL.Conexion.Con()))
+                {
+                    var query = "LibrioDelete";
+                    using (SqlCommand cmd=new SqlCommand())
+                    {
+                        cmd.Connection = connection;
+                        cmd.CommandText = query;
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        connection.Open();
+
+                        SqlParameter[] collection=new SqlParameter[1];
+
+                        collection[0] = new SqlParameter("IdLibro", SqlDbType.Int);
+                        collection[0].Value = IdLibro;
+
+
+                        cmd.Parameters.AddRange(collection);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected >= 1)
+                        {
+                            result.Message = ("Se elimino el dato correctamente");
+                        }
+                    }
+                }
+                result.Correct = true;
+            }
+
             catch (Exception ex)
             {
                 result.Ex = ex;
--- PL/Program.cs
+++ PL/Program.cs
@@ -10,32 +10,39 @@ namespace PL
     {
         public static void Main(string[] args)
         {
-            Add();
+            //Add();
+            Delete();
             
         }
 
         public static void Add()
         {
             ML.Libro libro = new ML.Libro();
+
             Console.WriteLine("Ingresa los datos");
+
             Console.WriteLine("Ingresa el nombre");
             libro.Nombre=Console.ReadLine();
+
             Console.WriteLine("Ingresa el Numero de Paginas");
             libro.NumeroPaginas = int.Parse(Console.ReadLine());
+
             Console.WriteLine("Ingresa la Fecha de Publicacion");
             libro.FechaPublicacion = Console.ReadLine();
+            
             Console.WriteLine("Ingresa la edicion");
             libro.Edicion = Console.ReadLine();
+            
             libro.Autor = new ML.Autor();
             Console.WriteLine("Ingresa el autor");
             libro.Autor.IdAutor = int.Parse(Console.ReadLine());
 
             libro.Editorial = new ML.Editorial();
-            Console.WriteLine("Ingresa el Numero de Paginas");
+            Console.WriteLine("Ingresa la editorial");
             libro.Editorial.IdEditorial = int.Parse(Console.ReadLine());
 
             libro.Genero = new ML.Genero();
-            Console.WriteLine("Ingresa la Fecha de Publicacion");
+            Console.WriteLine("Ingresa el Genero");
             libro.Genero.IdGenero = int.Parse(Console.ReadLine());
 
             ML.Result result = BL.Libro.Add(libro);
@@ -44,5 +51,25 @@ namespace PL
                 Console.WriteLine(result.Message);
             }
         }
+
+
+        public static void Delete()
+        {
+            ML.Libro libro = new ML.Libro();
+
+            Console.WriteLine("Ingresa los datos");
+
+            Console.WriteLine("Ingresa el IdLibro para que se eliminado");
+            libro.IdLibro = int.Parse(Console.ReadLine());
+
+
+            ML.Result result = BL.Libro.Delete(libro.IdLibro);
+            if (result.Correct)
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+
     }
 }
